d0ff7e4f4d0cb091a3685cde597ed6b32d3b238a
DotCipher <dotcipher@mail.dev>
2023-12-04 17:36:57 +0100

    first commit

---
+++ app/src/main/java/com/dotcipher/gift_discovery/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dotcipher.gift_discovery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
